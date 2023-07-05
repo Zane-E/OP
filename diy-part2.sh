2d725226ecaa1b05ad50cd1e5e36f59a708ae6b5
Demon <72865351+Zane-E@users.noreply.github.com>
2023-07-05 20:53:51 +0800

    commit message

---
+++ diy-part2.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# poweroff
+git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+
+#主题
+rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/kenzo/luci-app-argon-config
+git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
+
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/kenzo/luci-theme-argon
+git clone -b 18.06 https://github.com/SpeedPartner/luci-theme-argon-18.06-patch.git feeds/luci/themes/luci-theme-argon
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+
+# 修改概览里时间显示为中文数字
+sed -i 's/os.date()/os.date("%Y年%m月%d日") .. " " .. translate(os.date("%A")) .. " " .. os.date("%X")/g' package/lean/autocore/files/x86/index.htm
+
+#微信推送
+rm -rf feeds/kenzo/luci-app-serverchan
+rm -rf feeds/luci/applications/luci-app-serverchan
+git clone -b openwrt-18.06 https://github.com/tty228/luci-app-serverchan.git feeds/luci/applications/luci-app-serverchan
+# 修改欢迎banner
+cp -f $GITHUB_WORKSPACE/banner package/base-files/files/etc/banner
+
+./scripts/feeds update -a
+./scripts/feeds install -a
